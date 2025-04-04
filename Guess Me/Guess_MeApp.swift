ff5c1a8b9d2e357df4367b1436d44297c73d647d
afshinshafei <afshin.shafei@gmail.com>
2025-04-04 10:22:59 +0200

    Initial Commit

---
+++ Guess Me/Guess_MeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_MeApp.swift
+//  Guess Me
+//
+//  Created by Afshin on 04/04/25.
+//
+
+import SwiftUI
+
+@main
+struct Guess_MeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Guess MeTests/Guess_MeTests.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_MeTests.swift
+//  Guess MeTests
+//
+//  Created by Afshin on 04/04/25.
+//
+
+import Testing
+@testable import Guess_Me
+
+struct Guess_MeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
